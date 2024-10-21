c12d1728c61cc82bd5a4b05e3c17a119b80f5094
AnthonyCointre <8732@holbertonstudents.com>
2024-10-21 18:23:28 +0200

    modify scipt.js: no comments

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,19 +1,13 @@
-// Fonction pour afficher les onglets (Login et Sign Up)
 function showTab(tab) {
   const tabs = document.querySelectorAll('.tab');
   const titles = document.querySelectorAll('.tab-title');
-
   tabs.forEach(t => {
-    t.classList.remove('active'); // Masque tous les onglets
+    t.classList.remove('active');
   });
-
   titles.forEach(title => {
-    title.style.display = 'none'; // Masque tous les titres
+    title.style.display = 'none';
   });
-
-  document.getElementById(tab).classList.add('active'); // Affiche l'onglet sélectionné
-
-  // Affiche le titre correspondant
+  document.getElementById(tab).classList.add('active');
   if (tab === 'login') {
     document.getElementById('login-title').style.display = 'block';
   } else if (tab === 'sign-up') {
@@ -21,11 +15,8 @@ function showTab(tab) {
   }
 }
 
-
-
-// Créer une balise <script> pour le widget Calendly
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = 'https://assets.calendly.com/assets/external/widget.js';
-script.async = true; // Charger le script de manière asynchrone
-document.body.appendChild(script); // Ajouter la balise <script> à la fin du <body>
+script.async = true;
+document.body.appendChild(script);
