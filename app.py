43801705eb10165512601592b3acbfbff6ff047c
AnthonyCointre <8732@holbertonstudents.com>
2024-10-21 17:31:16 +0200

    modify app.py

---
+++ app.py
@@ -0,0 +1,78 @@
+import json
+import bcrypt
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+
+def load_users():
+    with open('data/users.json', 'r') as f:
+        return json.load(f)
+
+
+def save_users(users):
+    with open('data/users.json', 'w') as f:
+        json.dump(users, f, indent=2)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    users = load_users()  # Charger les utilisateurs existants
+    if request.method == 'POST':
+        # Vérifier l'action (login ou sign-up)
+        action = request.form.get('action')
+        username = request.form['username']
+        password = request.form['password']
+
+        if action == 'login':  # Action de connexion
+            if username in users['users'] and bcrypt.checkpw(password.encode('utf-8'), users['users'][username].encode('utf-8')):
+                # Stocker l'utilisateur dans la session
+                session['username'] = username
+                # Rediriger vers la page d'accueil
+                return redirect(url_for('home'))
+            else:
+                return render_template('login.html', error='Invalid username or password.')
+
+        elif action == 'sign-up':  # Action d'inscription
+            if username in users['users']:
+                return render_template('login.html', error='Username already exists.')
+
+            # Hacher le mot de passe et enregistrer le nouvel utilisateur
+            hashed_password = bcrypt.hashpw(
+                password.encode('utf-8'), bcrypt.gensalt())
+            users['users'][username] = hashed_password.decode('utf-8')
+            save_users(users)  # Sauvegarder les nouveaux utilisateurs
+
+            # Connexion automatique de l'utilisateur après l'inscription
+            # Stocker l'utilisateur dans la session
+            session['username'] = username
+            # Rediriger vers la page d'accueil
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
